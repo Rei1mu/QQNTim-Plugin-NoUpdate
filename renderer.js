865d5b473524b1075c2d6da6bb66677ee146f2ab
Rei1mu <www.3850@qq.com>
2023-07-13 16:58:44 +0800

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -6,9 +6,8 @@ module.exports.default = class Entry {
                 const update_btn = document.querySelector("body > div.q-dialog > div.update-dialog.q-dialog-main > div.q-dialog-footer > div > button.q-button.q-button--primary.q-button--default > span");
                 if (update_btn) {
                     clearInterval(interval);
-                    if (update_btn.textContent == "立即更新") {
+                    if (update_btn.textContent == "立即更新")
                         document.querySelector("body > div.q-dialog > div.update-dialog.q-dialog-main > div.q-dialog-header > i").click()
-                    }
                 }
             }, 100);
             setInterval(() => {
