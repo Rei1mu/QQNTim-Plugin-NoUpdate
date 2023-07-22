9bc712c54ed1791b27866324717515d7e74d1d95
Rei1mu <www.3850@qq.com>
2023-07-22 15:21:39 +0800

    for plugin v3

--- renderer.js
+++ renderer.js
@@ -1,16 +1,14 @@
 
-module.exports.default = class Entry {
-    constructor(qqntim) {
-        qqntim.windowLoadPromise.then(() => {
-            const interval = setInterval(() => {
-                let update_btn = document.querySelector("body > div.q-dialog > div.update-dialog.q-dialog-main > div.q-dialog-footer > div > button.q-button.q-button--primary.q-button--default > span");
-                if (!update_btn)
-                    return;
-                clearInterval(interval);
-                if (update_btn.textContent == "立即更新")
-                    document.querySelector("body > div.q-dialog > div.update-dialog.q-dialog-main > div.q-dialog-header > i").click()
-            }, 100);
-            setInterval(() => { clearInterval(interval); }, 60000)
-        });
-    }
+const qqntim = require("qqntim/renderer")
+qqntim.windowLoadPromise.then(() => {
+    const interval = setInterval(() => {
+        let update_btn = document.querySelector("body > div.q-dialog > div.update-dialog.q-dialog-main > div.q-dialog-footer > div > button.q-button.q-button--primary.q-button--default > span");
+        if (!update_btn)
+            return;
+        clearInterval(interval);
+        if (update_btn.textContent == "立即更新")
+            document.querySelector("body > div.q-dialog > div.update-dialog.q-dialog-main > div.q-dialog-header > i").click()
+    }, 100);
+    setInterval(() => { clearInterval(interval); }, 60000)
 }
+);
